1dd2f9991479c290b054a20d4725bc3f317effda
anastaciem <91271992+anastaciem@users.noreply.github.com>
2021-09-24 16:37:15 +0300

    Перша версія

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+// Lab_02.cpp
+// < Демчук Анастасія >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 14
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+	z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+	z2 = tan(2 * a) + 1 / cos(2 * a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
